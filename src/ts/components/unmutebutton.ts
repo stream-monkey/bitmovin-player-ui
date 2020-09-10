52be5bd038c1eaa89b67ad9a8ec0c2359deb17e5
Alex Coleman <alex@selftaughtcoders.com>
2020-09-10 13:53:11 -0700

    fixed unmute button positioning

--- src/ts/components/unmutebutton.ts
+++ src/ts/components/unmutebutton.ts
@@ -10,7 +10,7 @@ import { AdEvent, LinearAd, PlayerAPI } from 'bitmovin-player';
 export interface UnmuteButtonConfig extends ButtonConfig {
   /**
    * The delay in milliseconds after which the unmute button will be hidden when there is no user interaction.
-   * Default: 7 seconds (7000)
+   * Default: 5 seconds (5000)
    */
   hideDelay?: number;
 }
@@ -38,35 +38,58 @@ export class UnmuteButton extends Button<UnmuteButtonConfig> {
     let playerConfig = player.getConfig();
 
     let isMuted = player.isMuted();
-    let isAutoplay = playerConfig.playback && playerConfig.playback.autoplay
-      && playerConfig.playback.autoplay === true;
-
-    // Not muted & auto-played? Just abort.
-    if ( ! (isAutoplay && isMuted)) {
+    // let isAutoplay = playerConfig.playback && playerConfig.playback.autoplay
+    //   && playerConfig.playback.autoplay === true;
+    
+    // Not muted? Just abort.
+    if ( ! isMuted) {
       return;
     }
 
-    let hideTimeout = new Timeout(config.hideDelay, () => {
+    // @NOTE: I'm doing the "autoplay" check below with timestamps,
+    // because our player doesn't use the playback.autoplay setting
+    // for some strange UI conflict reason.
+    // 
+    // // Not muted & auto-played? Just abort.
+    // if ( ! (isAutoplay && isMuted)) {
+    //   return;
+    // }
+
+    let hideAfterFadeOutTimeout = new Timeout(1000, () => {
       this.hide();
+    });
+
+    let hideTimeout = new Timeout(config.hideDelay, () => {
+      this.getDomElement().addClass('bmpui-fadeout');
+      
+      hideAfterFadeOutTimeout.start();
     });
 
     let showDelay = new Timeout(500, () => {
       this.show();
     });
 
+    let readyTime = 0;
+
     let readyHandler = () => {
-      // // Tiny delay to showing to avoid an ugly overlap
-      // // w/ the play button fade out/animation.
-      // showDelay.start();
+      readyTime = Date.now();
     }
 
-    let playingHandler = () => {
-      // Tiny delay to showing to avoid an ugly overlap
-      // w/ the play button fade out/animation.
-      showDelay.start();
+    let playingTime = 0;
 
-      // Once it starts playing, start the auto-hide timeout.
-      hideTimeout.start();
+    let playingHandler = () => {
+      playingTime = Date.now();
+
+      // If the video was played within a second of the player being ready,
+      // assume "autoplay", and show the unmute button.
+      if ((playingTime - readyTime) <= 1000) {
+        // Tiny delay to showing to avoid an ugly overlap
+        // w/ the play button fade out/animation.
+        showDelay.start();
+
+        // Once it starts playing, start the auto-hide timeout.
+        hideTimeout.start();
+      }
     }
 
     // If unmuted, hide the button.
@@ -77,6 +100,8 @@ export class UnmuteButton extends Button<UnmuteButtonConfig> {
     player.on(player.exports.PlayerEvent.Ready, readyHandler);
     player.on(player.exports.PlayerEvent.Playing, playingHandler);
     player.on(player.exports.PlayerEvent.Unmuted, unmutedHandler);
+    player.on(player.exports.PlayerEvent.Paused, unmutedHandler);
+    player.on(player.exports.PlayerEvent.PlaybackFinished, unmutedHandler);
 
     this.onClick.subscribe(() => {
       player.unmute();
